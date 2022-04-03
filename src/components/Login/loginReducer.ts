858513fb5081af08995698efe6e95a3c32e63c1f
ole4ka140592 <ole4ka140592@gmail.com>
2022-04-04 01:41:03 +0300

    add changes in loginReducer

--- src/components/Login/loginReducer.ts
+++ src/components/Login/loginReducer.ts
@@ -1,7 +1,6 @@
 import {Dispatch} from "redux";
 import {cardsApi, LoginType} from "../../api/api";
-import {ActionsType} from "../../bll/store";
-import {registrationActions} from "../Registration/RegistrationBLL/registration-reducer";
+import {ActionsType, AppThunk} from "../../bll/store";
 
 
 const initialState = {
@@ -11,7 +10,7 @@ const initialState = {
 
 export type InitialStateType = typeof initialState
 
-export const loginReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+export const loginReducer = (state: InitialStateType = initialState, action: LoginActionsType): InitialStateType => {
     switch (action.type) {
         case 'login/SET-IS-LOGGED-IN':
             return {...state, isLoggedIn: action.value}
@@ -31,7 +30,7 @@ export const setLoginError = (error: string) =>
     ({type: 'login/SET-ERROR', error} as const)
 
 // thunk
-export const loginTC = (data: LoginType) => (dispatch: Dispatch<ActionsType>) => {
+export const loginTC = (data: LoginType): AppThunk => dispatch => {
     cardsApi.login(data)
         .then((res) => {
             dispatch(setIsLoggedInAC(true))

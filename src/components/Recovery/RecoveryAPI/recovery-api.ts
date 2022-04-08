9e8d10bb04a97aa8fa803d94da3c0384c771f702
stacy <ada.davis27@gmail.com>
2022-04-08 23:16:05 +0300

    changes

--- src/components/Recovery/RecoveryAPI/recovery-api.ts
+++ src/components/Recovery/RecoveryAPI/recovery-api.ts
@@ -11,7 +11,7 @@ export const recoveryAPI = {
     toSendInstructions(email: string) {
         return instance.post<any, AxiosResponse<ResponseType>, RecoveryDataType>('auth/forgot', {
             email, from: '<ada.davis27@gmail.com>',
-            message: `<div style="background-color: #d0eca1; padding: 20px; border-radius: 15px">BRAINSTORM: This is a password recovery link: <a href='http://localhost:3000/cards#/new-password/$token$'>link</a></div>`
+            message: `<div style="background-color: #d0eca1; padding: 20px; border-radius: 15px">BRAINSTORM: This is a password recovery link: <a href='https://stacy-imladris.github.io/cards#/new-password/$token$'>link</a></div>`
         }).then(res => res.data)
     },
 }

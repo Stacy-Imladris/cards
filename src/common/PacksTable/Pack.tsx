d261f42a99f8574750e34e1e7f5c84a3c90882f1
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-04-09 22:25:12 +0300

    Fix: change isMyPacks value in Pack.tsx - it should be userId equal to pack.user_id

--- src/common/PacksTable/Pack.tsx
+++ src/common/PacksTable/Pack.tsx
@@ -1,19 +1,22 @@
 import {FC} from 'react'
 import {PackActions} from './PackActions'
 import {PackType} from '../../bll/packs-reducer';
+import {useAppSelector} from '../../bll/store'
 
 type PackPropsType = {
     pack: PackType
 }
 
 export const Pack: FC<PackPropsType> = ({pack}) => {
+    const userId = useAppSelector(state => state.profile.user._id)
     return <tr>
         <td>{pack.name}</td>
         <td>{pack.cardsCount}</td>
         <td>{pack.updated}</td>
         <td>{pack.user_name}</td>
         <td>
-            <PackActions key={pack._id} isMyPacks={pack.private}/>
+            <PackActions key={pack._id} isMyPacks={pack.user_id === userId}/>
+            {/*<PackActions key={pack._id} isMyPacks={true}/>*/}
         </td>
     </tr>
 

8f4c1f0de5413566d80cc008eb09e29d057ca5e5
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-04-10 08:15:31 +0300

    Fix: Date format in Pack.jsx

--- src/components/PacksTable/Pack.tsx
+++ src/components/PacksTable/Pack.tsx
@@ -2,17 +2,22 @@ import {FC} from 'react'
 import {PackActions} from './PackActions'
 import {useAppSelector} from '../../bll/store'
 import {PackType} from '../../api/packs-api';
+import {getLastUpdatedDate} from '../../utils/date-helpers'
 
 type PackPropsType = {
     pack: PackType
 }
 
+
+
 export const Pack: FC<PackPropsType> = ({pack}) => {
     const userId = useAppSelector(state => state.profile.user._id)
+    const lastUpdate = getLastUpdatedDate(pack.updated)
+
     return <tr>
         <td>{pack.name}</td>
         <td>{pack.cardsCount}</td>
-        <td>{pack.updated}</td>
+        <td>{lastUpdate}</td>
         <td>{pack.user_name}</td>
         <td>
             <PackActions key={pack._id} isMyPacks={pack.user_id === userId}/>

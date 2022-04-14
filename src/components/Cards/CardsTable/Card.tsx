b2e37f5f6b86b7c420315a4f9e845854942e30d7
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-04-14 16:30:36 +0300

    Add logic for Card changes

--- src/components/Cards/CardsTable/Card.tsx
+++ src/components/Cards/CardsTable/Card.tsx
@@ -1,17 +1,59 @@
-import {FC, memo} from 'react'
+import {FC, memo, useState} from 'react'
 import {getLastUpdatedDate} from '../../../utils/date-helpers'
-import {CardType} from '../../../api/cards-api';
+import {CardType, UpdateCardPayload} from '../../../api/cards-api'
+import {useDispatch} from 'react-redux'
+import {updateCard} from '../../../bll/cards-reducer'
 
 type CardPropsType = {
     card: CardType
 }
 
 export const Card: FC<CardPropsType> = memo(({card}) => {
+    const [isEditQuestion, setIsEditQuestion] = useState<boolean>(false)
+    const [question, setQuestion] = useState<string>(card.question)
+    const [isEditAnswer, setIsEditAnswer] = useState<boolean>(false)
+    const [answer, setAnswer] = useState<string>(card.answer)
+
     const lastUpdate = getLastUpdatedDate(card.updated)
+    const dispatch = useDispatch()
+
+    const uploadQuestionChanges = () => {
+        setIsEditQuestion(false)
+        const updatedCard: UpdateCardPayload = {
+            card: {
+                _id: card._id,
+                question
+            }
+        }
+        dispatch(updateCard(updatedCard))
+    }
+
+    const uploadAnswerChanges = () => {
+        setIsEditAnswer(false)
+        const updatedCard: UpdateCardPayload = {
+            card: {
+                _id: card._id,
+                answer
+            }
+        }
+        dispatch(updateCard(updatedCard))
+    }
 
     return <tr>
-        <td>{card.question}</td>
-        <td>{card.answer}</td>
+        {isEditQuestion
+            ? <td><input onChange={(e) => setQuestion(e.currentTarget.value)}
+                         onBlur={uploadQuestionChanges}
+                         value={question}/>
+            </td>
+            : <td onDoubleClick={() => setIsEditQuestion(true)}>{question}</td>
+        }
+        {isEditAnswer
+            ? <td><input onChange={(e) => setAnswer(e.currentTarget.value)}
+                         onBlur={uploadAnswerChanges}
+                         value={answer}/>
+            </td>
+            : <td onDoubleClick={() => setIsEditAnswer(true)}>{answer}</td>
+        }
         <td>{lastUpdate}</td>
         <td>{card.grade}</td>
     </tr>

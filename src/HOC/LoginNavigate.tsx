9fa168be7ffe1104650669fc4b3ea6c1b9d1feea
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-04-14 14:51:28 +0300

    Refactor HOC LoginNavigate.tsx

--- src/HOC/LoginNavigate.tsx
+++ src/HOC/LoginNavigate.tsx
@@ -2,9 +2,11 @@ import {selectIsLoggedIn} from '../selectors/selectors'
 import {Navigate} from 'react-router-dom'
 import {PATH} from '../app/AllRoutes'
 import {FC} from 'react'
+import {useAppSelector} from '../bll/store'
 
 export const LoginNavigate: FC = ({children}) => {
-    if (!selectIsLoggedIn) {
+    const isLoggedIn = useAppSelector(selectIsLoggedIn)
+    if (!isLoggedIn) {
         return <Navigate to={PATH.LOGIN} />
     }
     return <>{children}</>

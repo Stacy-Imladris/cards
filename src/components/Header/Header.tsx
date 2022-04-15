59c715298985411af4080e37b3ae3fe0d963945b
ole4ka140592 <ole4ka140592@gmail.com>
2022-04-15 20:50:06 +0300

    added changes to display myPacks and allPacksList

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,15 +7,18 @@ import {useAppSelector} from '../../bll/store';
 import {themeActions, ThemeType} from '../../bll/themeReducer';
 import {SuperSelect} from '../../common/super-components/c5-SuperSelect/SuperSelect';
 import {saveState} from '../../utils/localstorage';
-import {selectIsLoggedIn, selectTheme} from '../../selectors/selectors';
+import {selectIsLoggedIn, selectTheme, selectUser_id} from '../../selectors/selectors';
 import {logout} from '../Login/LoginBLL/loginReducer';
+import {packsActions} from "../Packs/packs-reducer";
+import {profileActions} from "../Profile/profile-reducer";
+import {UserType} from "../Profile/profile-api";
 
 const themes = ['day', 'night']
 
 export const Header = () => {
     const theme = useAppSelector(selectTheme)
     const isLoggedIn = useAppSelector(selectIsLoggedIn)
-
+    const user_id = useAppSelector(selectUser_id)
     const dispatch = useDispatch()
 
     const logOut = () => {
@@ -27,12 +30,20 @@ export const Header = () => {
         saveState(theme)
     }, [dispatch])
 
+    const onClickToProfile = () => {
+        dispatch(packsActions.setPacksForUser(user_id))
+    }
+
+    const onClickToPacksList = () => {
+        dispatch(packsActions.setPacksForUser(''))
+    }
+
     return (
         <nav className={s.main}>
             <div className={s.links}>
-                <NavLink to={PATH.PROFILE}
+                <NavLink to={PATH.PROFILE} onClick={onClickToProfile}
                          className={({isActive}) => isActive ? s.active : s.nav}>Profile</NavLink>
-                {isLoggedIn && <NavLink to={PATH.PACKS}
+                {isLoggedIn && <NavLink to={PATH.PACKS} onClick={onClickToPacksList}
                                          className={({isActive}) => isActive ? s.active : s.nav}>Packs List</NavLink>}
                 {!isLoggedIn && <NavLink to={PATH.REGISTRATION}
                                          className={({isActive}) => isActive ? s.active : s.nav}>Registration</NavLink>}

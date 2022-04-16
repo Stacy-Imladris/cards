514c996836f86decb4ff3d81dec133784d108c62
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-04-16 18:16:40 +0300

    Clean cards array state onBackPage click in Cards.tsx

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -2,27 +2,27 @@ import {CardsTable} from './CardsTable/CardsTable'
 import t from '../../common/styles/Themes.module.css'
 import s from './Cards.module.css'
 import c from '../../common/styles/Container.module.css'
-import {useAppSelector} from '../../bll/store';
-import {useCallback} from 'react';
-import {SearchField} from '../SearchField/SearchField';
-import {useNavigate} from 'react-router-dom';
-import {SuperButton} from '../../common/super-components/c2-SuperButton/SuperButton';
-import {useDispatch} from 'react-redux';
-import {addCard, cardsActions} from './cards-reducer';
+import {useAppSelector} from '../../bll/store'
+import {useCallback} from 'react'
+import {SearchField} from '../SearchField/SearchField'
+import {useNavigate} from 'react-router-dom'
+import {SuperButton} from '../../common/super-components/c2-SuperButton/SuperButton'
+import {useDispatch} from 'react-redux'
+import {addCard, cardsActions} from './cards-reducer'
 import {
     selectCardAnswer,
     selectCardQuestion, selectLoginError,
     selectPackName, selectPackUserId,
     selectTheme, selectUser_id
-} from '../../selectors/selectors';
-import {Notification} from "../../common/notification/Notification";
+} from '../../selectors/selectors'
+import {Notification} from '../../common/notification/Notification'
 
 export const Cards = () => {
     const theme = useAppSelector(selectTheme)
     const packName = useAppSelector(selectPackName)
     const cardQuestion = useAppSelector(selectCardQuestion)
     const cardAnswer = useAppSelector(selectCardAnswer)
-    const cardsPack_id = useAppSelector(state=> state.cards.params.cardsPack_id)
+    const cardsPack_id = useAppSelector(state => state.cards.params.cardsPack_id)
     const error = useAppSelector(selectLoginError)
     const userId = useAppSelector(selectUser_id)
     const packUserId = useAppSelector(selectPackUserId)
@@ -44,13 +44,18 @@ export const Cards = () => {
         dispatch(addCard({cardsPack_id: cardsPack_id}))
     }
 
+    const onBackPage = () => {
+        navigate(-1)
+        dispatch(cardsActions.setCards([]))
+    }
+
     return (
         <div className={s.cardsContainer}>
             <div className={`${c.container} ${t[theme + '-text']}`}>
                 <div className={s.cardsTable}>
                     <div className={s.backAndTitle}>
                         <SuperButton className={s.backIcon}
-                                     onClick={() => navigate(-1)}>
+                                     onClick={onBackPage}>
                             ←
                         </SuperButton>
                         <div className={c.title}>{packName}</div>
@@ -64,7 +69,8 @@ export const Cards = () => {
                             <SearchField value={cardAnswer} placeholder={'Enter answer'}
                                          onChangeWithDebounce={onChangeDebounceAnswerRequest}/>
                         </div>
-                        {userId === packUserId && <SuperButton className={c.addItem} onClick={addNewCard}>Add card</SuperButton>}
+                        {userId === packUserId &&
+                        <SuperButton className={c.addItem} onClick={addNewCard}>Add card</SuperButton>}
                     </div>
                     <div className={c.table}><CardsTable/></div>
                     {error && <Notification text={error}/>}

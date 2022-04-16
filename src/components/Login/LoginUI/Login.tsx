0dcb2a4531dceb7208586047c215dcf67ad7fa78
stacy <ada.davis27@gmail.com>
2022-04-17 01:01:18 +0300

    solved problem with checkbox

--- src/components/Login/LoginUI/Login.tsx
+++ src/components/Login/LoginUI/Login.tsx
@@ -1,5 +1,4 @@
 import {useCallback, useEffect, useState} from 'react';
-import k from './Login.module.css';
 import {useDispatch} from 'react-redux';
 import {useAppSelector} from '../../../bll/store';
 import {Link, Navigate} from 'react-router-dom';
@@ -9,15 +8,16 @@ import {PATH} from '../../../app/AllRoutes';
 import s from '../../../common/styles/Forms.module.css'
 import t from '../../../common/styles/Themes.module.css'
 import {Logo} from '../../../common/logo/Logo';
-import {Preloader} from "../../../common/preloader/Preloader";
-import {SuperCheckbox} from "../../../common/super-components/c3-SuperCheckbox/SuperCheckbox";
-import {selectIsLoggedIn, selectLoginError, selectLoginIsLoading, selectTheme
+import {Preloader} from '../../../common/preloader/Preloader';
+import {SuperCheckbox} from '../../../common/super-components/c3-SuperCheckbox/SuperCheckbox';
+import {
+    selectIsLoggedIn, selectLoginError, selectLoginIsLoading, selectTheme
 } from '../../../selectors/selectors';
 import {login, loginActions} from '../LoginBLL/loginReducer';
 
 export const Login = () => {
-    const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
     const [rememberMe, setRememberMe] = useState<boolean>(false)
 
     const error = useAppSelector(selectLoginError)
@@ -55,25 +55,25 @@ export const Login = () => {
                 className={s.email}
             />
             <span>Password</span>
-            <div className={s.password}><SuperInputText
-                value={password}
-                onChangeText={setPassword}
-                onEnter={onClickLogin}
-                className={s.password}
-                eye
-            />
+            <div>
+                <SuperInputText value={password}
+                                onChangeText={setPassword}
+                                onEnter={onClickLogin}
+                                className={s.password} eye/>
             </div>
-            <span><SuperCheckbox checked={rememberMe}
-                                 onChangeChecked={setRememberMe}>
-                <span className={t[theme + '-text']}>Remember me</span>
-            </SuperCheckbox></span>
+            <div className={s.checkbox}>
+                <SuperCheckbox checked={rememberMe} onChangeChecked={setRememberMe}>
+                    Remember me
+                </SuperCheckbox>
+            </div>
             <div className={s.error}>{error}</div>
-            <div><Link to="/password-recovery" className={`${k.forgotPassword} ${t[theme + '-text']}`}>Forgot
-                Password</Link></div>
+            <Link to="/password-recovery"
+                  className={`${s.link} ${t[theme + '-text']}`}>
+                Forgot Password</Link>
             <SuperButton onClick={onClickLogin} className={s.login}>Login</SuperButton>
-            <div><Link to="/registration" className={`${k.dontHaveAccount} ${t[theme + '-text']}`}>Don't have an
-                account?</Link></div>
-            <div><Link to="/registration" className={`${s.SignUp} ${t[theme + '-text']}`}>Sign Up</Link></div>
+            <span>Don't have an account?</span>
+            <Link to="/registration" className={`${s.link} ${t[theme + '-text']}`}>
+                Sign Up</Link>
         </div>
     )
 }

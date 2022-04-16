209093e754cfce3b7a8ed726dd73bed40b84c7b1
stacy <ada.davis27@gmail.com>
2022-04-16 22:40:19 +0300

    modal for delete css

--- src/components/DeletePackForm/DeletePackForm.tsx
+++ src/components/DeletePackForm/DeletePackForm.tsx
@@ -10,8 +10,12 @@ type DeletePackFormPropsType = {
     packId: string
     name: string
 }
-export const DeletePackForm: FC<DeletePackFormPropsType> = memo(({onClickNotOpen, isOpen, name, packId}) => {
-
+export const DeletePackForm: FC<DeletePackFormPropsType> = memo(({
+                                                                     onClickNotOpen,
+                                                                     isOpen,
+                                                                     name,
+                                                                     packId
+                                                                 }) => {
     const dispatch = useDispatch()
 
     const onClickDeletePack = useCallback(() => {
@@ -19,10 +23,13 @@ export const DeletePackForm: FC<DeletePackFormPropsType> = memo(({onClickNotOpen
         onClickNotOpen()
     }, [dispatch, onClickNotOpen, packId, name])
 
-    return <Modal onClickNotOpen={onClickNotOpen} width={400} height={300} isOpen={isOpen}>
+    return <Modal onClickNotOpen={onClickNotOpen} width={450} height={230}
+                  isOpen={isOpen}>
         <div>Do you really want to remove pack '{name}'?</div>
         <div>All cards will be excluded from this course.</div>
-        <SuperButton onClick={onClickNotOpen}>Cancel</SuperButton>
-        <SuperButton onClick={onClickDeletePack} red>Delete</SuperButton>
+        <div>
+            <SuperButton onClick={onClickNotOpen}>Cancel</SuperButton>
+            <SuperButton onClick={onClickDeletePack} red>Delete</SuperButton>
+        </div>
     </Modal>
 })

00cb004952d3963e07ca0a0b32f5d14ef4338e99
stacy <ada.davis27@gmail.com>
2023-01-27 15:05:48 +0300

    absolute imports

--- src/components/Packs/PacksUI/PacksTable/PacksList/Pack/Pack/Pack.tsx
+++ src/components/Packs/PacksUI/PacksTable/PacksList/Pack/Pack/Pack.tsx
@@ -1,13 +1,13 @@
 import {FC, memo} from 'react'
 import {PackActions} from './PackActions'
-import {PackType} from '../../../../../PacksAPI/packs-api'
+import {PackType} from 'components/Packs/PacksAPI/packs-api'
 import {useDispatch} from 'react-redux';
-import {cardsActions} from '../../../../../../Cards/CardsBLL/cards-reducer';
+import {cardsActions} from 'components/Cards/CardsBLL/cards-reducer';
 import {useNavigate} from 'react-router-dom';
-import {selectUser_id} from '../../../../../../../store/selectors';
-import {getLastUpdatedDate} from '../../../../../../../utils/getLastUpdatedDate';
-import {PATH} from '../../../../../../../enums/paths';
-import {useAppSelector} from '../../../../../../../store/store';
+import {selectUser_id} from 'store/selectors';
+import {getLastUpdatedDate} from 'utils/getLastUpdatedDate';
+import {PATH} from 'enums/paths';
+import {useAppSelector} from 'store/store';
 
 type PackPropsType = {
     pack: PackType

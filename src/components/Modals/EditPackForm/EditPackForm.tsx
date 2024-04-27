a04d17c4879a95b16c3494f1a93e58371b637a0e
stacy <ada.davis27@gmail.com>
2024-04-27 16:10:10 +0300

    refactoring

--- src/components/Modals/EditPackForm/EditPackForm.tsx
+++ src/components/Modals/EditPackForm/EditPackForm.tsx
@@ -5,39 +5,39 @@ import {Modal} from '../Modal/Modal';
 import {SuperButton} from 'common/super-components/c2-SuperButton/SuperButton';
 import {SuperInputText} from 'common/super-components/c1-SuperInputText/SuperInputText';
 
-type EditPackFormPropsType = {
-    onClickNotOpen: () => void
-    isOpen: boolean
-    packId: string
-    name: string
+type Props = {
+  onClickNotOpen: () => void
+  isOpen: boolean
+  packId: string
+  name: string
 }
-export const EditPackForm: FC<EditPackFormPropsType> = memo(({
-                                                                 onClickNotOpen,
-                                                                 isOpen,
-                                                                 name,
-                                                                 packId
-                                                             }) => {
-    const [newName, setNewName] = useState<string>(name)
+export const EditPackForm: FC<Props> = memo(({
+                                               onClickNotOpen,
+                                               isOpen,
+                                               name,
+                                               packId
+                                             }) => {
+  const [newName, setNewName] = useState<string>(name)
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const onClickUpdatePack = useCallback(() => {
-        dispatch(updatePack(packId, newName, name))
-        onClickCleanUpStates()
-    }, [dispatch, onClickNotOpen, packId, newName, name])
+  const onClickUpdatePack = useCallback(() => {
+    dispatch(updatePack(packId, newName, name))
+    onClickCleanUpStates()
+  }, [dispatch, onClickNotOpen, packId, newName, name])
 
-    const onClickCleanUpStates = () => {
-        onClickNotOpen()
-        setNewName(name)
-    }
+  const onClickCleanUpStates = () => {
+    onClickNotOpen()
+    setNewName(name)
+  }
 
-    return <Modal onClickNotOpen={onClickCleanUpStates} isOpen={isOpen}>
-        <div>Pack name</div>
-        <SuperInputText value={newName} placeholder={'Enter new name'}
-                        onChangeText={setNewName}/>
-        <div>
-            <SuperButton onClick={onClickCleanUpStates}>Cancel</SuperButton>
-            <SuperButton onClick={onClickUpdatePack}>Save</SuperButton>
-        </div>
-    </Modal>
+  return <Modal onClickNotOpen={onClickCleanUpStates} isOpen={isOpen}>
+    <div>Pack name</div>
+    <SuperInputText value={newName} placeholder={'Enter new name'}
+                    onChangeText={setNewName}/>
+    <div>
+      <SuperButton onClick={onClickCleanUpStates}>Cancel</SuperButton>
+      <SuperButton onClick={onClickUpdatePack}>Save</SuperButton>
+    </div>
+  </Modal>
 })

799bb8d8b1b5833a2852fa8b6d8ee54b5ddacd8f
stacy <ada.davis27@gmail.com>
2024-05-21 23:04:28 +0300

    refactoring

--- src/components/Cards/CardsUI/CardsTable/CardsList/CardsList.tsx
+++ src/components/Cards/CardsUI/CardsTable/CardsList/CardsList.tsx
@@ -2,14 +2,9 @@ import {FC, memo} from 'react';
 import {CardType} from 'components/Cards/CardsAPI/cards-api';
 import {Card} from './Card/Card';
 
-type CardsListPropsType = {
+type Props = {
     cards: CardType[]
 }
 
-export const CardsList: FC<CardsListPropsType> = memo(({cards}) => {
-    return <>
-        {
-            cards.map(card => <Card key={card._id} card={card}/>)
-        }
-    </>
-})
+export const CardsList: FC<Props> = memo(({cards}) =>
+    <>{cards.map(card => <Card key={card._id} card={card}/>)}</>)

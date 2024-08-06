b7ad599283758fb2e1331d3b6f472c7f11dcae72
stacy <ada.davis27@gmail.com>
2024-08-06 12:01:36 +0300

    refactoring

--- src/components/Modals/DeletePackForm/DeletePackForm.tsx
+++ src/components/Modals/DeletePackForm/DeletePackForm.tsx
@@ -1,34 +1,35 @@
-import {FC, memo, useCallback} from 'react';
-import {useDispatch} from 'react-redux';
-import {deletePack} from 'components/Packs/PacksBLL/packs-reducer';
-import {Modal} from '../Modal/Modal';
 import {SuperButton} from 'common/super-components/c2-SuperButton/SuperButton';
+import {deletePack} from 'components/Packs/PacksBLL/packs-reducer';
+import {memo, useCallback} from 'react';
+import {useDispatch} from 'react-redux';
+import {Modal} from '../Modal/Modal';
 
-type DeletePackFormPropsType = {
-    onClickNotOpen: () => void
-    isOpen: boolean
-    packId: string
-    name: string
+type Props = {
+  onClickNotOpen: () => void
+  isOpen: boolean
+  packId: string
+  name: string
 }
-export const DeletePackForm: FC<DeletePackFormPropsType> = memo(({
-                                                                     onClickNotOpen,
-                                                                     isOpen,
-                                                                     name,
-                                                                     packId
-                                                                 }) => {
-    const dispatch = useDispatch()
 
-    const onClickDeletePack = useCallback(() => {
-        dispatch(deletePack(packId, name))
-        onClickNotOpen()
-    }, [dispatch, onClickNotOpen, packId, name])
+export const DeletePackForm = memo(({
+                                      onClickNotOpen,
+                                      isOpen,
+                                      name,
+                                      packId
+                                    }: Props) => {
+  const dispatch = useDispatch()
 
-    return <Modal onClickNotOpen={onClickNotOpen} isOpen={isOpen}>
-        <div>Do you really want to remove pack '{name}'?</div>
-        <div>All cards will be excluded from this course.</div>
-        <div>
-            <SuperButton onClick={onClickNotOpen}>Cancel</SuperButton>
-            <SuperButton onClick={onClickDeletePack} red>Delete</SuperButton>
-        </div>
-    </Modal>
+  const onClickDeletePack = useCallback(() => {
+    dispatch(deletePack(packId, name))
+    onClickNotOpen()
+  }, [dispatch, onClickNotOpen, packId, name])
+
+  return <Modal onClickNotOpen={onClickNotOpen} isOpen={isOpen}>
+    <div>Do you really want to remove pack '{name}'?</div>
+    <div>All cards will be excluded from this course.</div>
+    <div>
+      <SuperButton onClick={onClickNotOpen}>Cancel</SuperButton>
+      <SuperButton onClick={onClickDeletePack} red>Delete</SuperButton>
+    </div>
+  </Modal>
 })

e19cedc86c10f51895dbcda7c963741c1a9c0892
stacy <ada.davis27@gmail.com>
2025-04-05 18:42:02 +0300

    refactoring

--- src/components/Auth/Registration/RegistrationUI/Registration.tsx
+++ src/components/Auth/Registration/RegistrationUI/Registration.tsx
@@ -8,58 +8,56 @@ import {Preloader} from 'common/preloader/Preloader';
 import {Logo} from 'common/logo/Logo';
 
 type Props = {
-    signUp: () => void
-    email: string
-    password: string
-    password2: string
-    setEmail: (value: string) => void
-    setPassword: (value: string) => void
-    setPassword2: (value: string) => void
-    error: string
-    isLoading: boolean
-    theme: string
-    navigate: NavigateFunction
+  signUp: () => void
+  email: string
+  password: string
+  password2: string
+  setEmail: (value: string) => void
+  setPassword: (value: string) => void
+  setPassword2: (value: string) => void
+  error: string
+  isLoading: boolean
+  theme: string
+  navigate: NavigateFunction
 }
 
 export const Registration = memo(({
-                                      signUp,
-                                      email,
-                                      setEmail,
-                                      password,
-                                      setPassword,
-                                      password2,
-                                      setPassword2,
-                                      error,
-                                      isLoading,
-                                      theme,
-                                      navigate
-                                  }: Props) => {
-    return (
-        <div className={`${s.container} ${t[theme + '-text']}`}>
-            <Logo/>
-            <div className={s.preloader}>{isLoading && <Preloader/>}</div>
-            <div className={s.mainText}>Sign up</div>
-            <span>Email</span>
-            <div>
-                <SuperInputText value={email} onChangeText={setEmail} onEnter={signUp}/>
-            </div>
-            <span>Password</span>
-            <div>
-                <SuperInputText value={password} onChangeText={setPassword}
-                                onEnter={signUp} eye/>
-            </div>
-            <span>Confirm password</span>
-            <div>
-                <SuperInputText value={password2} onChangeText={setPassword2}
-                                onEnter={signUp} eye/>
-            </div>
-            <div className={s.buttons}>
-                <SuperButton disabled={isLoading} onClick={() => navigate('/login')}>
-                    Cancel
-                </SuperButton>
-                <SuperButton disabled={isLoading} onClick={signUp}>Register</SuperButton>
-            </div>
-            <div className={s.error}>{error}</div>
-        </div>
-    )
-})
+                                    signUp,
+                                    email,
+                                    setEmail,
+                                    password,
+                                    setPassword,
+                                    password2,
+                                    setPassword2,
+                                    error,
+                                    isLoading,
+                                    theme,
+                                    navigate
+                                  }: Props) => (
+    <div className={`${s.container} ${t[theme + '-text']}`}>
+      <Logo/>
+      <div className={s.preloader}>{isLoading && <Preloader/>}</div>
+      <div className={s.mainText}>Sign up</div>
+      <span>Email</span>
+      <div>
+        <SuperInputText value={email} onChangeText={setEmail} onEnter={signUp}/>
+      </div>
+      <span>Password</span>
+      <div>
+        <SuperInputText value={password} onChangeText={setPassword}
+                        onEnter={signUp} eye/>
+      </div>
+      <span>Confirm password</span>
+      <div>
+        <SuperInputText value={password2} onChangeText={setPassword2}
+                        onEnter={signUp} eye/>
+      </div>
+      <div className={s.buttons}>
+        <SuperButton disabled={isLoading} onClick={() => navigate('/login')}>
+          Cancel
+        </SuperButton>
+        <SuperButton disabled={isLoading} onClick={signUp}>Register</SuperButton>
+      </div>
+      <div className={s.error}>{error}</div>
+    </div>
+))

c566fac5f6e6e72f4d12e86197e3dd30410c06d7
stacy <ada.davis27@gmail.com>
2025-06-02 02:52:16 +0300

    refactoring

--- src/common/logo/Logo.tsx
+++ src/common/logo/Logo.tsx
@@ -1,4 +1,4 @@
-import s from './Logo.module.css';
+import styles from './Logo.module.css';
 import logo from 'assets/images/logo.png'
 import t from 'common/styles/Themes.module.css';
 import {selectTheme} from 'store/selectors';
@@ -8,7 +8,7 @@ export const Logo = () => {
     const theme = useAppSelector(selectTheme)
 
     return <>
-        <img alt={'logo'} src={logo} className={s.logo}/>
-        <h2 className={`${s.brand} ${t[theme + '-text']}`}>BRAINSTORM</h2>
+        <img alt={'logo'} src={logo} className={styles.logo}/>
+        <h2 className={`${styles.brand} ${t[theme + '-text']}`}>BRAINSTORM</h2>
     </>
 }

f6e5ff69800f8d63d381eddb916df2fff81f3c28
stacy <ada.davis27@gmail.com>
2025-06-17 09:42:38 +0300

    refactoring

--- src/common/scroll/Scroll.tsx
+++ src/common/scroll/Scroll.tsx
@@ -3,23 +3,25 @@ import s from './Scroll.module.css'
 import {SuperButton} from '../super-components/c2-SuperButton/SuperButton';
 
 export const Scroll = () => {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const handleScroll = useCallback(() => {
-        if (window.scrollY > 100) setShow(true);
-        else setShow(false);
-    }, [])
+  const handleScroll = useCallback(() => {
+    if (window.scrollY > 100) setShow(true);
+    else setShow(false);
+  }, [])
 
-    const scrollFunction = () => {
-        window.scrollTo({top: 0, behavior: 'smooth'})
-    }
+  const scrollFunction = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'})
+  }
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll)
-    }, []);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, []);
 
-    return <>
+  return (
+      <>
         {show && <SuperButton className={s.scroll} onClick={scrollFunction}>⃤</SuperButton>}
-    </>
+      </>
+  )
 }

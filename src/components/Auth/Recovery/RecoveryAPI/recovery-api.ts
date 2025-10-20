e250cd63bf8478b48d96e898e8d929b5f793b6d4
stacy <ada.davis27@gmail.com>
2025-10-20 12:11:01 +0300

    refactoring

--- src/components/Auth/Recovery/RecoveryAPI/recovery-api.ts
+++ src/components/Auth/Recovery/RecoveryAPI/recovery-api.ts
@@ -4,14 +4,14 @@ import {ResponseType} from '../../Registration/RegistrationAPI/registration-api'
 
 export const recoveryAPI = {
     async toSendInstructions(email: string) {
-        return instance.post<any, AxiosResponse<ResponseType>, RecoveryDataType>('auth/forgot', {
+        return instance.post<any, AxiosResponse<ResponseType>, RecoveryData>('auth/forgot', {
             email, from: '<ada.davis27@gmail.com>',
             message: `<div style="background-color: #d0eca1; padding: 20px; border-radius: 15px">BRAINSTORM: This is a password recovery link: <a href='https://stacy-imladris.github.io/cards#/new-password/$token$'>link</a></div>`
         }).then(res => res.data)
     },
 }
 
-export type RecoveryDataType = {
+export type RecoveryData = {
     email: string
     from: string
     message: string

e197b5052730dd9a4edb209ced12477ef6353bb0
stacy <ada.davis27@gmail.com>
2025-11-04 22:03:26 +0300

    refactoring

--- src/components/Auth/Recovery/RecoveryUI/Recovery.tsx
+++ src/components/Auth/Recovery/RecoveryUI/Recovery.tsx
@@ -8,51 +8,54 @@ import {SuperInputText} from 'common/super-components/c1-SuperInputText/SuperInp
 import {Logo} from 'common/logo/Logo';
 
 type Props = {
-    email: string
-    setEmail: (value: string) => void
-    toSendInstructions: () => void
-    isLoading: boolean
-    error: string
-    check: boolean
-    theme: string
+  email: string
+  setEmail: (value: string) => void
+  toSendInstructions: () => void
+  isLoading: boolean
+  error: string
+  check: boolean
+  theme: string
 }
 
 export const Recovery = memo(({
-                                  setEmail,
-                                  email,
-                                  toSendInstructions,
-                                  isLoading,
-                                  error,
-                                  check,
-                                  theme
+                                setEmail,
+                                email,
+                                toSendInstructions,
+                                isLoading,
+                                error,
+                                check,
+                                theme
                               }: Props) => {
-    return check
-        ? <div className={`${styles.container} ${extraStyles[theme + '-text']}`}>
-            <Logo/>
-            <div className={styles.mainText}>Check Email</div>
-            <span>We've sent an Email with instructions to {email}</span>
-        </div>
-        : <div className={`${styles.container} ${extraStyles[theme + '-text']}`}>
-            <Logo/>
-            <div className={styles.preloader}>{isLoading && <Preloader/>}</div>
-            <div className={styles.mainText}>Forgot your password?</div>
-            <span>Email</span>
-            <div>
+  return (
+      <div className={`${styles.container} ${extraStyles[theme + '-text']}`}>
+        <Logo/>
+        {check
+            ? <>
+              <div className={styles.mainText}>Check Email</div>
+              <span>We've sent an Email with instructions to {email}</span>
+            </>
+            : <>
+              <div className={styles.preloader}>{isLoading && <Preloader/>}</div>
+              <div className={styles.mainText}>Forgot your password?</div>
+              <span>Email</span>
+              <div>
                 <SuperInputText value={email} onChangeText={setEmail}
                                 onEnter={toSendInstructions}/>
-            </div>
-            <span>Enter your email address and we will send you further instructions</span>
-            <div className={styles.buttons}>
+              </div>
+              <span>Enter your email address and we will send you further instructions</span>
+              <div className={styles.buttons}>
                 <SuperButton disabled={isLoading} onClick={toSendInstructions}>
-                    Send instructions
+                  Send instructions
                 </SuperButton>
-            </div>
-            <span>Did you remember your password?</span>
-            <div className={styles.error}>{error}</div>
-            <div>
+              </div>
+              <span>Did you remember your password?</span>
+              <div className={styles.error}>{error}</div>
+              <div>
                 <Link to="/login" className={`${styles.link} ${extraStyles[theme + '-text']}`}>
-                    Try logging in
+                  Try logging in
                 </Link>
-            </div>
-        </div>
+              </div>
+            </>}
+      </div>
+  )
 })

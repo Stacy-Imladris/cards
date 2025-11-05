28f2291f10a9ee58df2fa688b8f0fda982db6eb8
stacy <ada.davis27@gmail.com>
2025-11-05 12:11:01 +0300

    refactoring

--- src/components/Auth/Recovery/RecoveryUI/Recovery.tsx
+++ src/components/Auth/Recovery/RecoveryUI/Recovery.tsx
@@ -17,45 +17,35 @@ type Props = {
   theme: string
 }
 
-export const Recovery = memo(({
-                                setEmail,
-                                email,
-                                toSendInstructions,
-                                isLoading,
-                                error,
-                                check,
-                                theme
-                              }: Props) => {
-  return (
-      <div className={`${styles.container} ${extraStyles[theme + '-text']}`}>
-        <Logo/>
-        {check
-            ? <>
-              <div className={styles.mainText}>Check Email</div>
-              <span>We've sent an Email with instructions to {email}</span>
-            </>
-            : <>
-              <div className={styles.preloader}>{isLoading && <Preloader/>}</div>
-              <div className={styles.mainText}>Forgot your password?</div>
-              <span>Email</span>
-              <div>
-                <SuperInputText value={email} onChangeText={setEmail}
-                                onEnter={toSendInstructions}/>
-              </div>
-              <span>Enter your email address and we will send you further instructions</span>
-              <div className={styles.buttons}>
-                <SuperButton disabled={isLoading} onClick={toSendInstructions}>
-                  Send instructions
-                </SuperButton>
-              </div>
-              <span>Did you remember your password?</span>
-              <div className={styles.error}>{error}</div>
-              <div>
-                <Link to="/login" className={`${styles.link} ${extraStyles[theme + '-text']}`}>
-                  Try logging in
-                </Link>
-              </div>
-            </>}
-      </div>
-  )
-})
+export const Recovery = memo(({setEmail, email, toSendInstructions, isLoading, error, check, theme}: Props) => (
+    <div className={`${styles.container} ${extraStyles[theme + '-text']}`}>
+      <Logo/>
+      {check
+          ? <>
+            <div className={styles.mainText}>Check Email</div>
+            <span>We've sent an Email with instructions to {email}</span>
+          </>
+          : <>
+            <div className={styles.preloader}>{isLoading && <Preloader/>}</div>
+            <div className={styles.mainText}>Forgot your password?</div>
+            <span>Email</span>
+            <div>
+              <SuperInputText value={email} onChangeText={setEmail}
+                              onEnter={toSendInstructions}/>
+            </div>
+            <span>Enter your email address and we will send you further instructions</span>
+            <div className={styles.buttons}>
+              <SuperButton disabled={isLoading} onClick={toSendInstructions}>
+                Send instructions
+              </SuperButton>
+            </div>
+            <span>Did you remember your password?</span>
+            <div className={styles.error}>{error}</div>
+            <div>
+              <Link to="/login" className={`${styles.link} ${extraStyles[theme + '-text']}`}>
+                Try logging in
+              </Link>
+            </div>
+          </>}
+    </div>
+))

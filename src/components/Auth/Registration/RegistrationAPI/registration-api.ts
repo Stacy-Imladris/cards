423a1d729831892f2e4e0d2079bc1a28c13ca224
stacy <ada.davis27@gmail.com>
2025-11-24 18:30:17 +0300

    refactoring

--- src/components/Auth/Registration/RegistrationAPI/registration-api.ts
+++ src/components/Auth/Registration/RegistrationAPI/registration-api.ts
@@ -2,13 +2,13 @@ import {AxiosResponse} from 'axios';
 import {instance} from '../../../../api/instance';
 
 export const registrationAPI = {
-    signUp(data: Omit<RegDataType, 'password2'>) {
-        return instance.post<ResponseType, AxiosResponse<ResponseType>, Omit<RegDataType, 'password2'>>('auth/register', data)
+    signUp(data: Omit<RegData, 'password2'>) {
+        return instance.post<ResponseType, AxiosResponse<ResponseType>, Omit<RegData, 'password2'>>('auth/register', data)
             .then(res => res.data)
     },
 }
 
-export type RegDataType = {
+export type RegData = {
     email: string
     password: string
     password2: string

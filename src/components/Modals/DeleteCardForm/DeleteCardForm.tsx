34e3012d0e6ace1d4c2d1a90c77e254b34bbd640
stacy <ada.davis27@gmail.com>
2026-03-08 19:38:36 +0300

    refactoring

--- src/components/Modals/DeleteCardForm/DeleteCardForm.tsx
+++ src/components/Modals/DeleteCardForm/DeleteCardForm.tsx
@@ -5,23 +5,25 @@ import {SuperButton} from 'common/super-components/c2-SuperButton/SuperButton';
 import {deleteCard} from 'components/Cards/CardsBLL/cards-reducer';
 
 type Props = {
-    onClickNotOpen: () => void
-    isOpen: boolean
-    cardId: string
+  onClickNotOpen: () => void
+  isOpen: boolean
+  cardId: string
 }
 export const DeleteCardForm = memo(({onClickNotOpen, isOpen, cardId}: Props) => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const onClickDeleteCard = useCallback(() => {
-        dispatch(deleteCard(cardId))
-        onClickNotOpen()
-    }, [dispatch, onClickNotOpen, cardId])
+  const onClickDeleteCard = useCallback(() => {
+    dispatch(deleteCard(cardId))
+    onClickNotOpen()
+  }, [dispatch, onClickNotOpen, cardId])
 
-    return <Modal onClickNotOpen={onClickNotOpen} isOpen={isOpen}>
+  return (
+      <Modal onClickNotOpen={onClickNotOpen} isOpen={isOpen}>
         <div>Do you really want to remove card?</div>
         <div>
-            <SuperButton onClick={onClickNotOpen}>Cancel</SuperButton>
-            <SuperButton onClick={onClickDeleteCard} red>Delete</SuperButton>
+          <SuperButton onClick={onClickNotOpen}>Cancel</SuperButton>
+          <SuperButton onClick={onClickDeleteCard} red>Delete</SuperButton>
         </div>
-    </Modal>
+      </Modal>
+  )
 })

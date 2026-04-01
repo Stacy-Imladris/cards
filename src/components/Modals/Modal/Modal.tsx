3f7ea03a423b30d82748937385689efac51beb83
stacy <ada.davis27@gmail.com>
2026-04-01 21:50:43 +0300

    refactoring

--- src/components/Modals/Modal/Modal.tsx
+++ src/components/Modals/Modal/Modal.tsx
@@ -2,7 +2,7 @@ import {CSSProperties, FC, memo} from 'react';
 import styles from './Modal.module.css'
 import ReactDOM from 'react-dom';
 import {selectTheme} from '../../../store/selectors';
-import t from '../../../common/styles/Themes.module.css';
+import extraStyles from '../../../common/styles/Themes.module.css';
 import {useAppSelector} from '../../../store/store';
 
 type Props = {
@@ -20,7 +20,7 @@ export const Modal: FC<Props> = memo(({onClickNotOpen, isOpen, children, backgro
       ReactDOM.createPortal(
           <>
             <div style={{...backgroundStyle}} className={styles.background} onClick={onClickNotOpen}/>
-            <div style={{...modalStyle}} className={`${styles.modal} ${t[theme]} ${t[theme + '-text']}`}>
+            <div style={{...modalStyle}} className={`${styles.modal} ${extraStyles[theme]} ${extraStyles[theme + '-text']}`}>
               <div className={styles.escape}>
                 <div onClick={onClickNotOpen}>✘</div>
               </div>

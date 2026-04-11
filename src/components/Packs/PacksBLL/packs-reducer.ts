6739c73b2f58f76b507b9004c6151cfea4bf438f
stacy <ada.davis27@gmail.com>
2026-04-11 14:45:17 +0300

    refactoring

--- src/components/Packs/PacksBLL/packs-reducer.ts
+++ src/components/Packs/PacksBLL/packs-reducer.ts
@@ -42,28 +42,21 @@ export const packsReducer = (state: PacksInitialStateType = packsInitialState, a
 }
 
 export const packsActions = {
-    setPacks: (packs: PackType[]) =>
-        ({type: 'PACKS/SET_PACKS', payload: {packs}} as const),
-    setPacksForUser: (user_id: string) =>
-        ({type: 'PACKS/SET_PACKS_FOR_USER', payload: {user_id}} as const),
+    setPacks: (packs: PackType[]) => ({type: 'PACKS/SET_PACKS', payload: {packs}} as const),
+    setPacksForUser: (user_id: string) => ({type: 'PACKS/SET_PACKS_FOR_USER', payload: {user_id}} as const),
     setPacksMin: (min: number) => ({type: 'PACKS/SET_PACKS_MIN', payload: {min}} as const),
     setPacksMax: (max: number) => ({type: 'PACKS/SET_PACKS_MAX', payload: {max}} as const),
     setCardPacksTotalCount: (cardPacksTotalCount: number) =>
         ({type: 'PACKS/SET_CARD_PACKS_TOTAL_COUNT', payload: {cardPacksTotalCount}} as const),
-    setCurrentPage: (page: number) =>
-        ({type: 'PACKS/SET_CURRENT_PAGE', payload: {page}} as const),
-    setTitleForSearch: (packName: string) =>
-        ({type: 'PACKS/SET_TITLE_FOR_SEARCH', payload: {packName}} as const),
-    setSortParameters: (sortPacks: string) =>
-        ({type: 'PACKS/SET_SORT_PARAMETERS', payload: {sortPacks}} as const),
+    setCurrentPage: (page: number) => ({type: 'PACKS/SET_CURRENT_PAGE', payload: {page}} as const),
+    setTitleForSearch: (packName: string) => ({type: 'PACKS/SET_TITLE_FOR_SEARCH', payload: {packName}} as const),
+    setSortParameters: (sortPacks: string) => ({type: 'PACKS/SET_SORT_PARAMETERS', payload: {sortPacks}} as const),
     setMinCardsCount: (minCardsCount: number) =>
         ({type: 'PACKS/SET_PACKS_MIN_CARDS_COUNT', payload: {minCardsCount}} as const),
     setMaxCardsCount: (maxCardsCount: number) =>
         ({type: 'PACKS/SET_PACKS_MAX_CARDS_COUNT', payload: {maxCardsCount}} as const),
-    setPacksPageCount: (pageCount: number) =>
-        ({type: 'PACKS/SET_PACKS_PAGE_COUNT', payload: {pageCount}} as const),
-    setPacksType: (packsType: PacksType)=>
-        ({type: 'PACKS/SET_PACKS_TYPE', payload: {packsType}} as const),
+    setPacksPageCount: (pageCount: number) => ({type: 'PACKS/SET_PACKS_PAGE_COUNT', payload: {pageCount}} as const),
+    setPacksType: (packsType: PacksType)=> ({type: 'PACKS/SET_PACKS_TYPE', payload: {packsType}} as const),
 }
 
 //thunks

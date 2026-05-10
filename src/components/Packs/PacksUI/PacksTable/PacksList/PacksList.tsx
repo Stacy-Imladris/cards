ed6eab3897f7d2596fd8b3e81a6bbb4dc0f8b36b
stacy <ada.davis27@gmail.com>
2026-05-10 12:11:01 +0300

    refactoring

--- src/components/Packs/PacksUI/PacksTable/PacksList/PacksList.tsx
+++ src/components/Packs/PacksUI/PacksTable/PacksList/PacksList.tsx
@@ -1,10 +1,9 @@
 import {memo} from 'react'
 import {Pack} from './Pack/Pack/Pack'
-import {PackType} from 'components/Packs/PacksAPI/packs-api';
+import type {PackType} from 'components/Packs/PacksAPI/packs-api';
 
 type Props = {
     cardPacks: PackType[]
 }
 
-export const PacksList = memo(({cardPacks}: Props) =>
-    <>{cardPacks.map(pack => <Pack key={pack._id} pack={pack}/>)}</>)
+export const PacksList = memo(({cardPacks}: Props) => <>{cardPacks.map(pack => <Pack key={pack._id} pack={pack}/>)}</>)

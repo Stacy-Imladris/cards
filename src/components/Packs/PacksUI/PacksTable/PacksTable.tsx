a17738ef5044b80c158391d6b92133e6b64cf4ea
stacy <ada.davis27@gmail.com>
2026-05-19 13:17:00 +0300

    refactoring

--- src/components/Packs/PacksUI/PacksTable/PacksTable.tsx
+++ src/components/Packs/PacksUI/PacksTable/PacksTable.tsx
@@ -4,73 +4,75 @@ import {getPacks, packsActions} from 'components/Packs/PacksBLL/packs-reducer'
 import {useDispatch} from 'react-redux'
 import styles from 'common/styles/Table.module.css'
 import {
-    selectCardPacksTotalCount,
-    selectMaxForCards,
-    selectMinForCards,
-    selectPackNameForSearch,
-    selectPacks,
-    selectPackUserId,
-    selectPageCountForPacks,
-    selectPageForPacks,
-    selectSortForPacks,
+  selectCardPacksTotalCount,
+  selectMaxForCards,
+  selectMinForCards,
+  selectPackNameForSearch,
+  selectPacks,
+  selectPackUserId,
+  selectPageCountForPacks,
+  selectPageForPacks,
+  selectSortForPacks,
 } from 'store/selectors';
 import {Paginator} from 'components/Features/Paginator/Paginator';
 import {PacksTableHeader} from './PacksTableHeader/PacksTableHeader';
 import {useAppSelector} from 'store/store';
 
 export const PacksTable = () => {
-    const packs = useAppSelector(selectPacks)
-    const packName = useAppSelector(selectPackNameForSearch)
-    const user_id = useAppSelector(selectPackUserId)
-    const sortPacks = useAppSelector(selectSortForPacks)
-    const min = useAppSelector(selectMinForCards)
-    const max = useAppSelector(selectMaxForCards)
-    const cardPacksTotalCount = useAppSelector(selectCardPacksTotalCount)
-    const page = useAppSelector(selectPageForPacks)
-    const pageCount = useAppSelector(selectPageCountForPacks)
+  const packs = useAppSelector(selectPacks)
+  const packName = useAppSelector(selectPackNameForSearch)
+  const user_id = useAppSelector(selectPackUserId)
+  const sortPacks = useAppSelector(selectSortForPacks)
+  const min = useAppSelector(selectMinForCards)
+  const max = useAppSelector(selectMaxForCards)
+  const cardPacksTotalCount = useAppSelector(selectCardPacksTotalCount)
+  const page = useAppSelector(selectPageForPacks)
+  const pageCount = useAppSelector(selectPageCountForPacks)
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const onPageChanged = useCallback((page: number) => {
-        dispatch(packsActions.setCurrentPage(page))
-        dispatch(getPacks())
-    }, [dispatch])
+  const onPageChanged = useCallback((page: number) => {
+    dispatch(packsActions.setCurrentPage(page))
+    dispatch(getPacks())
+  }, [dispatch])
 
-    const onChangeSetAmountOfPacks = useCallback((amountOfPacks: number) => {
-        dispatch(packsActions.setPacksPageCount(amountOfPacks))
-    }, [dispatch])
+  const onChangeSetAmountOfPacks = useCallback((amountOfPacks: number) => {
+    dispatch(packsActions.setPacksPageCount(amountOfPacks))
+  }, [dispatch])
 
-    useEffect(() => {
-        dispatch(getPacks())
-    }, [dispatch, packName, user_id, sortPacks, min, max, pageCount])
+  useEffect(() => {
+    dispatch(getPacks())
+  }, [dispatch, packName, user_id, sortPacks, min, max, pageCount])
 
 
-    return <div className={styles.tableContainer}>
+  return (
+      <div className={styles.tableContainer}>
         <table className={styles.table}>
-            <thead>
-            <tr>
-                <PacksTableHeader text={'Name'} param={'name'}/>
-                <PacksTableHeader text={'Cards'} param={'cardsCount'}/>
-                <PacksTableHeader text={'Updated'} param={'updated'}/>
-                <PacksTableHeader text={'Creator'} param={'user_name'}/>
-                <th className={styles.actions}>
-                    Actions
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            <PacksList cardPacks={packs}/>
-            <tr>
-                <td colSpan={5} className={styles.pagination}>
-                    <div>
-                        <Paginator onChangeSetAmountOfItems={onChangeSetAmountOfPacks}
-                                   onPageChanged={onPageChanged}
-                                   itemsTotalCount={cardPacksTotalCount}
-                                   pageCount={pageCount} page={page}/>
-                    </div>
-                </td>
-            </tr>
-            </tbody>
+          <thead>
+          <tr>
+            <PacksTableHeader text={'Name'} param={'name'}/>
+            <PacksTableHeader text={'Cards'} param={'cardsCount'}/>
+            <PacksTableHeader text={'Updated'} param={'updated'}/>
+            <PacksTableHeader text={'Creator'} param={'user_name'}/>
+            <th className={styles.actions}>
+              Actions
+            </th>
+          </tr>
+          </thead>
+          <tbody>
+          <PacksList cardPacks={packs}/>
+          <tr>
+            <td colSpan={5} className={styles.pagination}>
+              <div>
+                <Paginator onChangeSetAmountOfItems={onChangeSetAmountOfPacks}
+                           onPageChanged={onPageChanged}
+                           itemsTotalCount={cardPacksTotalCount}
+                           pageCount={pageCount} page={page}/>
+              </div>
+            </td>
+          </tr>
+          </tbody>
         </table>
-    </div>
+      </div>
+  )
 }

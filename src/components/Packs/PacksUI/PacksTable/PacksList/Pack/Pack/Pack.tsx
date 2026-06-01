881979c92c3f16afcd1da5d863c499fa88b4d05a
stacy <ada.davis27@gmail.com>
2026-06-01 23:20:00 +0300

    refactoring

--- src/components/Packs/PacksUI/PacksTable/PacksList/Pack/Pack/Pack.tsx
+++ src/components/Packs/PacksUI/PacksTable/PacksList/Pack/Pack/Pack.tsx
@@ -10,31 +10,33 @@ import {getLastUpdatedDate} from 'utils/getLastUpdatedDate';
 import {PackActions} from './PackActions'
 
 type Props = {
-    pack: PackType
+  pack: PackType
 }
 
 export const Pack = memo(({pack}: Props) => {
-    const userId = useAppSelector(selectUserId)
-    const lastUpdate = getLastUpdatedDate(pack.updated)
+  const userId = useAppSelector(selectUserId)
+  const lastUpdate = getLastUpdatedDate(pack.updated)
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    const openCard = () => {
-        dispatch(cardsActions.setQuestionForSearch(''))
-        dispatch(cardsActions.setAnswerForSearch(''))
-        dispatch(cardsActions.setPackName(pack.name))
-        dispatch(cardsActions.setPackId(pack._id))
-        navigate(`${PATH.CARDS}/${pack.user_id}`)
-    }
+  const openCard = () => {
+    dispatch(cardsActions.setQuestionForSearch(''))
+    dispatch(cardsActions.setAnswerForSearch(''))
+    dispatch(cardsActions.setPackName(pack.name))
+    dispatch(cardsActions.setPackId(pack._id))
+    navigate(`${PATH.CARDS}/${pack.user_id}`)
+  }
 
-    return <tr>
+  return (
+      <tr>
         <td onClick={openCard} style={{cursor: 'pointer'}}>{pack.name}</td>
         <td>{pack.cardsCount}</td>
         <td>{lastUpdate}</td>
         <td>{pack.user_name}</td>
         <td>
-            <PackActions isMyPacks={pack.user_id === userId} pack={pack} />
+          <PackActions isMyPacks={pack.user_id === userId} pack={pack}/>
         </td>
-    </tr>
+      </tr>
+  )
 })
